f7fc06fce4ae291490495efacdaa74b15f064903
JM14OK <jm14ok@cs.aau.dk@a256-gc1-09.srv.aau.dk>
2024-08-12 11:11:07 +0200

    More queries

--- priority_experiment/same_tables.sh
+++ priority_experiment/same_tables.sh
@@ -11,12 +11,11 @@ do
     echo "Executing ${QUERY}"
     cp ${QUERY} ../TableSearch/queries/
     TIME_POINT="$(($(date +%s) - ${START_TIME}))s"
-    sleep 1m
+    sleep 30s
 
     QUERY_FILE=(${QUERY//// })
     QUERY_ID=(${QUERY_FILE[1]//./ })
     QUERY_ID=${QUERY_ID[0]}
     mkdir -p ${RESULT_DIR}${TIME_POINT}/
     cp -r ../TableSearch/data/search_output/${QUERY_ID}/ ${RESULT_DIR}${TIME_POINT}/
-    sleep 1m
 done
