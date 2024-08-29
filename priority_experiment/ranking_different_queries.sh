1a0adee4b744116a6d5a08544f0790ceb91d71be
JM14OK <jm14ok@cs.aau.dk@a256-gc1-09.srv.aau.dk>
2024-08-29 16:02:06 +0200

    Bug fix

--- priority_experiment/ranking_different_queries.sh
+++ priority_experiment/ranking_different_queries.sh
@@ -6,7 +6,7 @@ RESULT_DIR="different_results/"
 LOG_FILE="../TableSearch/data/log.txt"
 THETIS_RESULT_DIR="../TableSearch/data/search_output/"
 START_TIME=$(date +%s)
-QUERY_LIMIT=10
+QUERY_LIMIT=50
 TABLES=$(ls corpus/ | wc -l)
 mkdir -p ${RESULT_DIR}
 sleep 5m
@@ -38,8 +38,6 @@ do
     do
         cp -r ${RESULT} ${RESULT_DIR}${TIME_POINT}/
     done
-
-    rm -r 
 done
 
 # Get the final ranking when progressiv indexing is done
